26798ac3a1b100018b755aaad38669317fa6cbf3
Faisal Khan <faisalkh01122@gmail.com>
2024-08-21 23:33:16 -0700

    set processing on rst and fooddetails form validation etc

--- src/components/Processing/Processing.jsx
+++ src/components/Processing/Processing.jsx
@@ -1,26 +1,28 @@
-
 const Processing = () => {
   return (
-    <div style={{ 
-      display: 'flex', 
-      justifyContent: 'center', 
-      alignItems: 'flex-start', 
-      height: '100vh', 
-      paddingTop: '100px', // Adjust as needed
-      backgroundColor: 'rgba(0, 0, 0, 0.4)', // Light blur effect
-      backdropFilter: 'blur(2px)', // Blur effect
-      position: 'fixed',
-      top: 0,
-      left: 0,
-      width: '100%',
-      zIndex: 1000
-    }}>
-      <img 
-        src="/loading-loader.gif" 
-        alt="Loading..." 
-        style={{ width: '100px' }} // Adjust the size as needed
+    <div
+      className=" flex items-center gap-2 justify-center h-[100vh] w-[100vw]"
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "flex-start",
+        height: "100vh",
+        paddingTop: "100px", // Adjust as needed
+        backgroundColor: "rgba(0, 0, 0, 0.4)", // Light blur effect
+        backdropFilter: "blur(2px)", // Blur effect
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100%",
+        zIndex: 1000,
+      }}
+    >
+      <img
+        src="/loading-loader.gif"
+        alt="Loading..."
+        style={{ width: "200px" }} // Adjust the size as needed
       />
-      <h1 className="text-xl">Processing ....</h1>
+      <h1 className="text-xl font-semibold text-white">Processing ....</h1>
     </div>
   );
 };
--- src/router.jsx
+++ src/router.jsx
@@ -1,11 +1,9 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Route } from "react-router-dom";
 import RootLayout from "./_root/RootLayout";
 import SearchPage from "./_root/Pages/SearchPage";
 import Offers from "./_root/Pages/OfferPage";
 import ContactUs from "./_root/Pages/ContactUs";
 import Terms from "./_root/Pages/TermsAndConditions";
-import Login from "./_root/Pages/Login";
-import SignUp from "./_root/Pages/SignUp";
 import BuyGiftCard from "./_root/Pages/BuyGiftCard";
 import MyGift from "./_root/Pages/MyGiftCard";
 import FavoriteStore from "./_root/Pages/FavoriteRestaurants";
@@ -29,6 +27,7 @@ import AllRestaurants from "./components/Restaurants/AllRestaurants";
 import RequestDineIn from "./components/Restaurants/RequestDineIn";
 import TakeAway from "./components/Cart/TakeAway";
 import ProductDetail from "./components/Product";
+import SignUp from "./components/SignUpPages/SignUp";
 
 const router = createBrowserRouter([
   {
@@ -39,125 +38,129 @@ const router = createBrowserRouter([
         path: "",
         element: <HomePage />,
       },
-			{
-				path: "search",
-        element: <SearchPage />,
-			},
-			{
-				path: "offers",
-        element: <Offers />,
-			},
-			{
-				path: "contact-us",
-        element: <ContactUs/>,
-			},
-			{
-				path: "terms",
-        element: <TermsAndConditions />,
-			},
-			{
-				path: "my-orders",
-        element: <MyOrder />,
-			},
-			{
-				path: "login",
-				element: <Login />,
-			},
-			{
-				path: "register",
-				element: <SignUp/>
-			},
+      //   <Route path="/signin" element={<SignUp />} />,
+      //   <Route path="/phone" element={<Loginwithphone />} />,
+      //   <Route path="/forget" element={<Forgotpassword />} />,
+      //   <Route path="/login" element={<Login />} />,
 
-			{
+      { path: "search", element: <SearchPage /> },
+      // 	{path: "sign-in",
+      // element: <SignUp />,
+      // 	},
+      {
+        path: "offers",
+        element: <Offers />,
+      },
+      {
+        path: "contact-us",
+        element: <ContactUs />,
+      },
+      {
+        path: "terms",
+        element: <TermsAndConditions />,
+      },
+      {
+        path: "my-orders",
+        element: <MyOrder />,
+      },
+      // {
+      //   path: "login",
+      //   element: <Login />,
+      // },
+      {
+        path: "register",
+        element: <SignUp />,
+      },
+
+      {
         path: "buy-gift-card",
         element: <BuyGiftCard />,
       },
-			{
-				path: "my-gift-cards",
+      {
+        path: "my-gift-cards",
         element: <MyGift />,
-			},
-			{
-				path: "favorite-foods",
+      },
+      {
+        path: "favorite-foods",
         element: <FavoriteFood />,
-			},
-			{
-				path: "address-book",
-        element: <DeliveryAddress/>,
-			},
-			{
-				path: "restaurants",
-        element: <AllRestaurants title='All'/>,
-				
-			},
-			{
-				path: "restaurant/:restaurantId",
-				element: <RestaurantsPage />,
-				children: [
-					{
-						path: "category/:categoryId",
-						element: <Restaurants />,
-					},
-				],
-			},
-			{
-				path: "dine-in",
-        element: <AllRestaurants title='Dine in'/>,
-			},
-			{
-				path: "register",
-				element: <Terms/>
-			},
+      },
+      {
+        path: "address-book",
+        element: <DeliveryAddress />,
+      },
+      {
+        path: "restaurants",
+        element: <AllRestaurants title="All" />,
+      },
+      {
+        path: "restaurant/:restaurantId",
+        element: <RestaurantsPage />,
+        children: [
+          {
+            path: "category/:categoryId",
+            element: <Restaurants />,
+          },
+        ],
+      },
+      {
+        path: "dine-in",
+        element: <AllRestaurants title="Dine in" />,
+      },
+      {
+        path: "register",
+        element: <Terms />,
+      },
 
-			{
-				path: "account",
-				element: <Profile />,
-			},
-			{
-				path: "category/:categoryId",
-				element: <Restaurants />,
-			},
-			{
-				path: "favorite-restaurant",
-				element: <FavoriteRestaurants />,
-			},
-			{
-				path: "*",
-				element: <Page404 />,
-			},
-			
-			{
-				path: "delivery-support",
-				element: <DeliverySupport />,
-			},
-			{
-				path: "privacy",
-				element: <Privacy />,
-			},
+      {
+        path: "account",
+        element: <Profile />,
+      },
+      {
+        path: "category/:categoryId",
+        element: <Restaurants />,
+      },
+      {
+        path: "favorite-restaurant",
+        element: <FavoriteRestaurants />,
+      },
+      {
+        path: "*",
+        element: <Page404 />,
+      },
 
-			{
-				path: "checkout",
-				element: <Checkout />,
-			},
-			{
-				path: "delivery",
-				element: <Checkout />,
-			},
-			{
-				path:"takeaway",
-				element:<TakeAway />
-			},
-			{
-				path:"transactions",
-				element:<MyTransactions/>
-			},
-			{
-				path: "my-dine-in-requests",
-				element: <RequestDineIn />,
-			},
-			{
-				path:"product-detail",
-				element: <ProductDetail/>
-			}
+      {
+        path: "delivery-support",
+        element: <DeliverySupport />,
+      },
+      {
+        path: "privacy",
+        element: <Privacy />,
+      },
+
+      {
+        path: "checkout",
+        element: <Checkout />,
+      },
+      {
+        path: "delivery",
+        element: <Checkout />,
+      },
+      {
+        path: "takeaway",
+        element: <TakeAway />,
+      },
+      {
+        path: "transactions",
+        element: <MyTransactions />,
+      },
+      {
+        path: "my-dine-in-requests",
+        element: <RequestDineIn />,
+      },
+      {
+        path: "product-detail",
+        element: <ProductDetail />,
+      },
     ],
   },
 ]);
